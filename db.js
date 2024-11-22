5fce9715712997cf0384341934467f2dac4b7617
moranavraham1 <120803146+moranavraham1@users.noreply.github.com>
2024-11-22 21:33:59 +0200

    frontend

--- db.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-mongoose
-    .connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.error('MongoDB connection error:', err));
-
-module.exports = mongoose;
