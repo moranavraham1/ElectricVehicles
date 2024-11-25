effe0990ebc178003edf25e34c4f3ce42bc4405c
moranavraham1 <120803146+moranavraham1@users.noreply.github.com>
2024-11-25 21:01:36 +0200

    api

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'http://localhost:3001/api/auth';
 
 
 export const registerUser = async (fullName, email, password) => {
