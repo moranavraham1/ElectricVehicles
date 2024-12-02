dbb6809d0712a79c010947a99d20bf6ba374b8ff
moranavraham1 <120803146+moranavraham1@users.noreply.github.com>
2024-12-02 13:30:46 +0200

    Adding fields to the registration page

--- backend/User.js
+++ backend/User.js
@@ -2,15 +2,23 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-    fullName: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
+    phone: { type: String, required: true },
     password: { type: String, required: true },
-});
+}, { timestamps: true });
 
+// הצפנת סיסמה לפני שמירת המסמך
 UserSchema.pre('save', async function (next) {
     if (!this.isModified('password')) return next();
     this.password = await bcrypt.hash(this.password, 10);
     next();
 });
+
+// מתודה לבדיקת סיסמה
+UserSchema.methods.comparePassword = async function (enteredPassword) {
+    return bcrypt.compare(enteredPassword, this.password);
+};
 
 module.exports = mongoose.model('User', UserSchema);
